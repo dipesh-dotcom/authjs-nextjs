2c7708ff2cd8592eef2aa86614ff4a8859ff4ceb
Dipesh Shrestha <dipeshsth00@gmail.com>
2025-09-27 23:55:45 +0545

    auth.js complete with credentials login and github login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,8 +2,14 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github";
 import prisma from "./prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { schema } from "./schema";
+import { encode as defaultEncode } from "next-auth/jwt";
+import { v4 as uuid } from "uuid";
 
+const adapter = PrismaAdapter(prisma);
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter,
   providers: [
     GitHub,
     Credentials({
@@ -12,8 +18,12 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         password: {},
       },
       authorize: async (credentials) => {
+        const validatedCredentials = schema.parse(credentials);
         const user = await prisma.user.findFirst({
-          where: { email: credentials.email, password: credentials.password },
+          where: {
+            email: validatedCredentials.email,
+            password: validatedCredentials.password,
+          },
         });
 
         if (!user) {
@@ -24,4 +34,36 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account?.provider === "credentials") {
+        token.credentials = true;
+      }
+      return token;
+    },
+  },
+  jwt: {
+    encode: async function (params) {
+      if (params.token?.credentials) {
+        const sessionToken = uuid();
+
+        if (!params.token.sub) {
+          throw new Error("No user ID found in token");
+        }
+
+        const createdSession = await adapter?.createSession?.({
+          sessionToken: sessionToken,
+          userId: params.token.sub,
+          expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+        });
+
+        if (!createdSession) {
+          throw new Error("Failed to create session");
+        }
+
+        return sessionToken;
+      }
+      return defaultEncode(params);
+    },
+  },
 });
