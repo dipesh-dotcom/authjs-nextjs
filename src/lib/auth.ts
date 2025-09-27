5fdd266be336c0fb842d7be849e45b050611a457
Dipesh Shrestha <dipeshsth00@gmail.com>
2025-09-27 22:34:04 +0545

    sign in using credential-hardcode data to check the authorization

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,25 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    GitHub,
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const email = "admin@dip.com";
+        const password = "1234";
+
+        if (credentials.email === email && credentials.password === password) {
+          return { email, password };
+        } else {
+          throw new Error("Invalid credentials");
+        }
+      },
+    }),
+  ],
 });
